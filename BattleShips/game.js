dabb42a6caf8cc264a3ce3be85b15714bb9868e5
ARPetrenko <ms.suslikmur@mail.ru>
2020-02-06 20:47:38 +0300

    Update game.js

--- BattleShips/game.js
+++ BattleShips/game.js
@@ -1,6 +1,6 @@
-let location1 = 5
-let location2 = 6
-let location3 = 7
+let location1 = Math.floor(Math.random()*7)
+let location2 = location1 + 1
+let location3 = location2 + 1
 let currentShot
 let shots = 0
 let hits = 0
@@ -8,12 +8,29 @@ let isSunk = false
 
 while (isSunk === false) {
   currentShot = Number(prompt('Insert number from 0 to 9:'))
-  shots++
-  if (currentShot === location1 || currentShot === location2 || currentShot === location3) {
-    hits++
+  if (currentShot <= 9 && currentShot >= 0){
+    shots++
+    if (currentShot === location1 || currentShot === location2 || currentShot === location3) {
+      hits++
+      alert('HIT!')
+    } else {
+      alert('MISS!')
+    }
+    if (currentShot === location1) {
+      location1 = 'killed'
+    }
+    if (currentShot === location2) {
+      location2 = 'killed'
+    }
+    if (currentShot === location3) {
+      location3 = 'killed'
+    }
+    if (hits === 3) {
+      isSunk = true
+      alert('WIN!')
+    }
+  } else {
+    alert('Mistake! Number must be from 0 to 9.')
   }
-  if (hits === 3) {
-    isSunk = true
-    alert('WIN!')
-  }
+
 }
