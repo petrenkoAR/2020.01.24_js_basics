6b53adfaa324f0001bb98310d1f90631f451dce8
ARPetrenko <ms.suslikmur@mail.ru>
2020-02-07 18:29:28 +0300

    *

---
+++ 04/animation.js
@@ -0,0 +1,21 @@
+function reset() {
+  window.location.reload()
+}
+let box = document.querySelector(".box")
+
+function paintItBlack() {
+  box.style.backgroundColor = 'black'
+}
+
+function makeCircle() {
+  box.style.borderRadius = '50%'
+}
+
+function move() {
+  box.style.marginLeft = '100px'
+  box.style.marginTop = '100px'
+}
+
+function makeBlinking() {
+  
+}
